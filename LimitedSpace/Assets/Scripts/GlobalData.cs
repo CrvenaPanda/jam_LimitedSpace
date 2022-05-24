868368809a95ce95def186fbf49a451db935c6dc
CrvenaPanda <dusosluka@gmail.com>
2022-05-24 20:44:15 +0200

    Added better player movement.

---
+++ LimitedSpace/Assets/Scripts/GlobalData.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalData
+{
+    public static List<float> zTrackPos = new List<float> {0.2f, 1.6f, 3.0f };
+}
--- LimitedSpace/Assets/Scripts/PlayerController.cs
+++ LimitedSpace/Assets/Scripts/PlayerController.cs
@@ -2,9 +2,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private void Start()
+    {
+        UpdateDesiredZPos();
+    }
+
     private void Update()
     {
-        MoveUpOrDown(Input.GetAxis("Vertical") * Time.deltaTime * _upDownSpeed);
+        DetectVerticalMoving();
+        MoveVertical();
+
+        // TODO: Remove
         if (IsSprinting())
         {
             Run(_sprintSpeed * Time.deltaTime);
@@ -12,6 +20,11 @@ public class PlayerController : MonoBehaviour
         }
 
         Run(_runSpeed * Time.deltaTime);
+    }
+
+    private void UpdateDesiredZPos()
+    {
+        _desiredZPos = GlobalData.zTrackPos[_trackIndex];
     }
 
     private bool IsSprinting()
@@ -24,12 +37,57 @@ public class PlayerController : MonoBehaviour
         this.transform.position += Vector3.right * speed;
     }
 
-    private void MoveUpOrDown(float speed)
+    private void DetectVerticalMoving()
     {
-        this.transform.position += Vector3.forward * speed;
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            ChangeTrack(1);
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            ChangeTrack(-1);
+        }
+    }
+
+    private void ChangeTrack(int indexDelta)
+    {
+        _trackIndex += indexDelta;
+        _trackIndex = Mathf.Clamp(_trackIndex, 0, 2);
+        UpdateDesiredZPos();
+    }
+
+    private void MoveVertical()
+    {
+        if (IsEqual(this.transform.position.z, _desiredZPos, 0.02f))
+        {
+            return;
+        }
+
+        float direction = 1.0f;
+        if (this.transform.position.z > _desiredZPos)
+        {
+            direction = -1.0f;
+        }
+
+        this.transform.position += Vector3.forward * _runSpeed * direction * Time.deltaTime;
+    }
+
+    bool IsEqual(float a, float b, float epsilon)
+    {
+        if (a >= b - epsilon && a <= b + epsilon)
+        {
+            return true;
+        }
+
+        return false;
     }
 
     [SerializeField] private float _runSpeed = 10f;
     [SerializeField] private float _sprintSpeed = 15f;
     [SerializeField] private float _upDownSpeed = 7.5f;
+
+    private int _trackIndex = 1;
+    private float _desiredZPos;
 }
